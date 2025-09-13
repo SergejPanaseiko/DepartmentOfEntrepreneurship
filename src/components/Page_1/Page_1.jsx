04d09577df0c42d68e4e33bc91179acad039d4bf
Sergey <konsaltpns@gmail.com>
2025-09-13 18:14:52 +0300

    comm_2

--- src/components/Page_1/Page_1.jsx
+++ src/components/Page_1/Page_1.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import './Page_1.module.css';
 
 const Page_1 = () => {
   return (
     <div>
-          <h2>Тема 1. Предмет і зміст курсу "Економіка підприємства"</h2>
-          <h3 className='tema_1_zadinie_1'>Завдання 1</h3>
-
+      <h2>Тема 1. Предмет і зміст курсу "Економіка підприємства"</h2>
+      <h3 className="tema_1_zadinie_1">Завдання 1</h3>
     </div>
   );
 }
