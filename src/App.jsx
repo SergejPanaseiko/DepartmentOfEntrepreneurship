047384091fa3096357b9e865b3c600e4838cf8c6
Sergey <konsaltpns@gmail.com>
2025-09-13 15:33:41 +0300

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,7 @@ function App() {
   return (
     <>
       <Header />
-      <div>
-        <a
-          href="http://umsf.dp.ua/struktura/2021-03-09-08-36-26/kafedra-pidpryiemnytstva-ta-ekonomiky-pidpryiemstva"
-          target="_blank"
-        >
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a
-          href="http://umsf.dp.ua/struktura/2021-03-09-08-36-26/kafedra-pidpryiemnytstva-ta-ekonomiky-pidpryiemstva"
-          target="_blank"
-        >
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount((count) => (count = 0))}>
-          count Обнулить
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,13 @@ import s from './Header.module.css'
 const Header = () => {
   return (
     <div>
+      <a href="https://department-of-entrepreneurship.vercel.app/">Загрузка</a>
+
       <a
         className={s.logo}
         href="http://umsf.dp.ua/struktura/2021-03-09-08-36-26/kafedra-pidpryiemnytstva-ta-ekonomiky-pidpryiemstva"
       >
-        <img src={pict1} alt="" className={s.logo_pict} />        
+        {/* <img src={pict1} alt="" className={s.logo_pict} /> */}
         <h2 className={s.header_logo_main}>
           КАФЕДРА ПІДПРИЄМНИЦТВА, ТОРГІВЛІ ТА ЕКОНОМІКИ ПІДПРИЄМСТВА
         </h2>
@@ -18,9 +20,6 @@ const Header = () => {
 
       <p>Чи можеш бути бізнесменом? </p>
       <p>Чи можеш бути керівником компанії?</p>
-     
-     
-      
     </div>
   );
 }
